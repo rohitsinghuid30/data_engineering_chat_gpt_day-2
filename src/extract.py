fef9d8e22f62b15a54437137ddfab2eafc842364
rohit singh <rohituid30@gmail.com>
2026-09-17 05:35:23 +0530

    day-2

---
+++ src/extract.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+
+
+def extract_employees(engine):
+    query="select * from employees"
+    emp_df=pd.read_sql_query(query, engine)
+    return emp_df
+
+
+def extract_departments(engine):
+    query="select * from departments"
+    dept_df=pd.read_sql_query(query, engine)
+    return dept_df
